6f126acfd99e5637af6cbd4ad034ba6a35a04a9e
OleksiiProkoshin <my.mail2210@gmail.com>
2023-06-23 17:44:01 +0300

    SL done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ original, preview, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`)
+  .join("");
+
+galleryList.innerHTML = galleryMarkup;
+
+galleryList.addEventListener("click", handleGalleryClick);
+
+function handleGalleryClick(event) {
+  event.preventDefault();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250
+ });
+}
 console.log(galleryItems);
